5d51192e811ad6c7d3ca19b4dee7383d060d1708
Malek <malekgeshash@gmail.com>
2023-03-16 18:29:01 +0000

    V2

--- Rain.cpp
+++ Rain.cpp
@@ -4,12 +4,13 @@
 #include <iostream>
 #include <windows.h>
 #include <random>
-
-COORD coord;
-HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-CONSOLE_SCREEN_BUFFER_INFO info = {};
+#include <chrono>
 
 Rain::Rain() {
+    this->coord.Y = 0;
+    this->hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    this->info = {};
+    this->runtime = 10000;
     this->colors =
     {
         FOREGROUND_INTENSITY | FOREGROUND_GREEN
@@ -17,64 +18,61 @@ Rain::Rain() {
 
     this->characters = 
     {
-      '0', '1' ,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '
+      '0', '1' 
     };
 
-    GetConsoleScreenBufferInfo(hConsole, &info);
-    COORD* p = &info.dwMaximumWindowSize;
-    for (int i = 0; i < (p->X); i++) {
-        if(i%2 == 0) {
-            digit* d = new digit();
-            d->setX(i);
-            this->digits.push_back(*d);
-            /*digit d ;
-            d.setX(i);
-            this->digits.push_back(d);*/
-        }
+    this->NoOfDigits=10;
+    for (int i = 0; i < NoOfDigits; i++) {
+         digit d ;
+         d.setX(i);
+         this->digits.push_back(d);
     }
 }
 
-Rain::Rain(std::vector<digit> digits,std::vector<WORD> colors,std::vector<char> characters) {
+Rain::Rain(std::vector<digit> digits,std::vector<WORD> colors,std::vector<char> characters, COORD coord, HANDLE hConsole, CONSOLE_SCREEN_BUFFER_INFO info, int NoOfDigits,int runtime) {
 	this->digits = digits;
     this->colors = colors;
     this->characters = characters;
+    this->coord = coord;
+    this->hConsole = hConsole;
+    this->info = info;
+    this->NoOfDigits = NoOfDigits;
+    this->runtime = runtime;
 }
 
 void Rain::setDigits(std::vector<digit> digits) {
 	this->digits = digits;
 }
 
-void Rain::digitalRain(int y,bool x) {
-    GetConsoleScreenBufferInfo(hConsole, &info);
-    COORD* p = &info.dwMaximumWindowSize;
-    int digitsIterator = 0;
-    int j = 0;
-    std::default_random_engine e;
-    std::uniform_int_distribution<int> u(0, 9);
-    e.seed((unsigned int)time(0));
-    for (int i = 0; i < p->X; i++) {
-        if(i % 2 == x) {
-            this->digits.at(digitsIterator).setX(i);
-            this->digits.at(digitsIterator).setColor(this->colors.at(j));
-            this->digits.at(digitsIterator).setNumber(u(e));
-            this->digits.at(digitsIterator).setCh(this->characters.at(u(e)));
-            if (j < this->colors.size() - 1) {
-                j++;
+void Rain::digitalRain() {
+    while (this->coord.Y<this->runtime) {
+        GetConsoleScreenBufferInfo(this->hConsole, &info);
+        COORD* p = &info.dwMaximumWindowSize;
+        int randomColNo = randomize(1, digits.size() - 1, 0);
+        for (int i = 0; i < randomColNo; i++) {
+            try {
+                this->digits.at(i).setX(randomize(0, p->X, i));
+                this->digits.at(i).setColor(this->colors.at(randomize(0, this->colors.size() - 1, i)));
+                this->digits.at(i).setCh(this->characters.at(randomize(0, this->characters.size() - 1, i)));
+                coord.X = this->digits.at(i).getX();
             }
-            else {
-                j = 0;
+            catch (std::out_of_range & e) {
+                std::cout << " Exception: " << e.what() << std::endl;
             }
-            coord.X = this->digits.at(digitsIterator).getX();
-            coord.Y = y;
             SetConsoleCursorPosition(hConsole, coord);
-            SetConsoleTextAttribute(hConsole, this->digits.at(digitsIterator).getColor());
-            std::cout << this->digits.at(digitsIterator);
-            if (digitsIterator < this->digits.size()-1) {
-                digitsIterator++;
-            }
+            SetConsoleTextAttribute(hConsole, this->digits.at(i).getColor());
+            std::cout << this->digits.at(i);
         }
+        coord.Y++;
+        Sleep(25);
     }
-   
-    }
+ }
+
+int Rain::randomize(int start, int end,int i) {
+    std::default_random_engine e;
+    std::uniform_int_distribution<int> u(start,end);
+    e.seed((unsigned int)time(0)+i);
+    return u(e);
+}
   
 
--- main.cpp
+++ main.cpp
@@ -4,62 +4,11 @@
 #include"digit.h"
 #include<vector>
 #include"Rain.h"
-void TestOutputs()
-{
-    int i = 0;
-    std::vector<WORD> colors = { FOREGROUND_BLUE ,FOREGROUND_GREEN,FOREGROUND_RED};
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    digit d(5,5,5,FOREGROUND_BLUE);
-    digit d2(6, 6, 6, FOREGROUND_GREEN);
-    digit d3(7, 7, 7, FOREGROUND_RED);
-    COORD coord; 
-    coord.X = d.getX();
-    coord.Y = d.getY();
-    SetConsoleCursorPosition(hConsole, coord);
-    SetConsoleTextAttribute(hConsole, d.getColor());
-    std::cout << d.getNumber();
-    while (1) {
-        static std::default_random_engine e;
-        static std::uniform_int_distribution<int> u(0, 9);
-        static std::default_random_engine e2;
-        static std::uniform_int_distribution<int> u2(0, 50);
-        static std::default_random_engine e3;
-        static std::uniform_int_distribution<int> u3(0, 50);
-        e.seed((unsigned int)time(0));
-        e2.seed((unsigned int)time(0));
-        e3.seed((unsigned int)time(0));
-        d.setNumber(u(e));
-        d.setX(u2(e2));
-        d.setY(u3(e3));
-        if (i < colors.size()-1) {
-            i++;
-        }
-        else {
-            i = 0;
-        }
-        d.setColor(colors.at(i));
-        coord.X = d.getX();
-        coord.Y = d.getY();
-        SetConsoleCursorPosition(hConsole, coord);
-        SetConsoleTextAttribute(hConsole, d.getColor());
-        std::cout << d.getNumber() << d2.getNumber() << d3.getNumber();
-        Sleep(1);
-    }
-    std::cout << d.getNumber();
-}
 
 int main(int argc, wchar_t* argv[])
 {
-
     Rain rain;
-    int  i = 0;
-    bool j = true;
-    while (1) {
-        rain.digitalRain(i,j);
-        Sleep(50);
-        i++;
-        j = !j;
-        
-    }
+    rain.digitalRain();
+
     return 0;
 }
